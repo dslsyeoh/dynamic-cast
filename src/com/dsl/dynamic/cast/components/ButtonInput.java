a586853007c1dc495976c6c019e9d4ee55ebe66e
dslsyeoh <syeoh91x@gmail.com>
2019-08-29 00:44:38 +0800

    add ButtonInput

---
+++ src/com/dsl/dynamic/cast/components/ButtonInput.java
@@ -0,0 +1,25 @@
+/*
+ * Owned by Steven Yeoh
+ * Copyright (c) 2019.
+ */
+
+package com.dsl.dynamic.cast.components;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+public class ButtonInput implements Component
+{
+    private Button button;
+
+    public ButtonInput()
+    {
+        button = new Button("Button");
+    }
+
+    @Override
+    public Node getNode()
+    {
+        return button;
+    }
+}
